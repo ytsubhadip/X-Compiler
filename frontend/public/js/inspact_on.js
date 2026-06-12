9513eb7800eaab0a7bf076ed7628d1b83c92a369
subhadip <ytsubhadip0099@gmail.com>
2026-06-12 22:56:38 +0530

    inspact on option off from all pages

---
+++ frontend/public/js/inspact_on.js
@@ -0,0 +1,13 @@
+document.addEventListener("contextmenu", (e) => {
+    e.preventDefault();
+});
+
+document.addEventListener("keydown", (e) => {
+    if (
+        e.key === "F12" ||
+        (e.ctrlKey && e.shiftKey && ["I", "J", "C"].includes(e.key.toUpperCase())) ||
+        (e.ctrlKey && e.key.toUpperCase() === "U")
+    ) {
+        e.preventDefault();
+    }
+});
